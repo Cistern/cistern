4847ed3e99ca3976c7fe04ac4b2e20896994834b
Preetam Jinka <pj@preet.am>
2016-03-30 23:55:20 -0400

    define Observation struct explicitly

--- state/series/observation.go
+++ state/series/observation.go
@@ -2,4 +2,20 @@ package series
 
 import "github.com/Cistern/catena"
 
-type Observation catena.Row
+type Observation struct {
+	Source    string
+	Metric    string
+	Timestamp int64
+	Value     float64
+}
+
+func (o Observation) toCatenaRow() catena.Row {
+	return catena.Row{
+		Source: o.Source,
+		Metric: o.Metric,
+		Point: catena.Point{
+			Timestamp: o.Timestamp,
+			Value:     o.Value,
+		},
+	}
+}

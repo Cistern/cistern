aebc2754261b8428f41f97001c12da7d56464464
Preetam Jinka <pj@preet.am>
2018-04-05 07:15:33 -0700

    more wip

--- internal/query/executor.go
+++ internal/query/executor.go
@@ -1,14 +1,47 @@
 package query
 
 // Executor is a query executor.
-type Executor struct{}
+type Executor struct {
+	table Table
+}
 
-func NewExecutor(t Table) *Executor {
-	return &Executor{}
+func NewExecutor(table Table) *Executor {
+	return &Executor{
+		table: table,
+	}
 }
 
 func (e *Executor) Execute(query Desc) error {
+	// Get a cursor
+	var cur Cursor
+	var err error
+	if len(query.Columns) == 0 && len(query.GroupBy) == 0 {
+		// No aggregations. Use a plain cursor.
+		cur, err = e.table.NewCursor()
+		if err != nil {
+			return err
+		}
+	} else {
+		fieldsMap := map[string]struct{}{}
+		for _, col := range query.Columns {
+			fieldsMap[col.Name] = struct{}{}
+		}
+		for _, col := range query.GroupBy {
+			fieldsMap[col.Name] = struct{}{}
+		}
+		fields := make([]string, 0, len(fieldsMap))
+		for field := range fieldsMap {
+			fields = append(fields, field)
+		}
+		cur, err = e.table.NewCursorForFields(fields)
+		if err != nil {
+			return err
+		}
+	}
+
 	// TODO
+	_ = cur
+
 	return nil
 }
 
